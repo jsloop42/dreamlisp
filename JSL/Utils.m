aedb287eb27fa01a3f946cf076748d18040d3cb6
jsloop <jsloop42@gmail.com>
2019-05-12 17:12:44 +0530

    Add documentation to Utils

--- JSL/Utils.m
+++ JSL/Utils.m
@@ -10,16 +10,19 @@
 
 @implementation Utils
 
+/** Checks if the given string matches the compiled regex pattern. */
 + (BOOL)matchString:(NSString *)string withExpression:(NSRegularExpression *)pattern {
     NSArray *matches = [pattern matchesInString:string options:0 range:NSMakeRange(0, [string length])];
     return ![matches isEmpty];
 }
 
+/** Checks if the given string matches the string pattern. */
 + (BOOL)matchString:(NSString *)string withPattern:(NSString *)pattern {
     NSRange range = [string rangeOfString:pattern options:NSRegularExpressionSearch range:NSMakeRange(0, [string length])];
     return range.location != NSNotFound;
 }
 
+/** Returns current timestamp in millisecond. */
 + (double)timestamp {
     return (double)(NSTimeInterval)[[NSDate date] timeIntervalSince1970] * 1000;
 }
